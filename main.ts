e64c08e1f8e93507c82b1394230860f601996f1c
Gnlow <gnlow@protonmail.com>
2024-04-04 13:58:24 +0900

    init

---
+++ main.ts
@@ -0,0 +1,18 @@
+import cytoscape from "https://esm.sh/cytoscape@3.28.1"
+
+const $container = document.createElement("div")
+document.body.append($container)
+$container.setAttribute("style", "height: 100%;")
+
+const cy = cytoscape({
+    container: $container,
+    elements: [
+        { data: { id: "a" } },
+        { data: { id: "b" } },
+        { data: {
+            id: "ab",
+            source: "a",
+            target: "b",
+        } },
+    ]
+})
